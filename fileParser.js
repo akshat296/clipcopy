d3aac22988fde943f8469230fc7eb8932a1f4db5
Akshat Tandon <tandon.akshat@thinksys.com>
2019-05-29 18:06:12 +0530

    changes made

--- fileParser.js
+++ fileParser.js
@@ -9,28 +9,94 @@ const directoryPath = document.getElementById('folderPath').value;
 let filesList;
 const walkSync = (dir, filelist = []) => {
     fs.readdirSync(dir).forEach(file => {
-  
+     
       filelist = fs.statSync(path.join(dir, file)).isDirectory()
         ? walkSync(path.join(dir, file), filelist)
-        : filelist.concat(path.join(dir, file));
+        : (file.slice(-4) === '.cfm' && filelist.concat(path.join(dir, file))) || filelist;
   
     });
   return filelist;
   }
   filesList = walkSync(directoryPath);
-  let strFilesList = filesList.reduce((a,v,i)=>a+v+"\n",'');
-
+  //filesList = ['C:\\opalsUtility\\coldfusion\\folder2\\folder4\\abc.cfm']
+  let strFilesList = filesList.reduce((a,v,i)=>a + v + "\n",'');
+  //strlist = "C:\\opalsUtility\\coldfusion\\folder2\\folder4\\abc.cfm"
   document.getElementById('files').innerHTML = strFilesList;
-  let contents;
-  filesList.forEach((v)=>{
-    contents = fs.readFileSync(v, 'utf8');
-    setTimeout(()=>{
-        document.getElementById('code').innerHTML = contents.replace(/&/g, '&amp;').replace(/</g, '&lt;').match('/<\s*a[^>]*>(.*?)<\s*/\s*a>/g')[0];
-    },2000)
+
+  var codeAnalysis = {
+    codeParser : function(contents){
+      var regexForCfquery = RegExp('<cfquery[^>].*?<\s*\/\s*cfquery>','isg');
+      let arrOfCfquery = contents.match(regexForCfquery)
+      return arrOfCfquery || [];
+    }
+  }
+ 
+
+  async function waitter(arrOfCfquery,idx){
+    await arrOfCfquery.reduce(async (a,query,i)=>{
+      const workers =  (q,id) => new Promise((res,rej)=>{
+        setTimeout(()=>{
+        document.getElementById('code').innerHTML = query.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+        console.log(q);
+        res(1)
+        },10*(idx*100)*i)
+      });
+      
+     
+       
+       await workers(query,i)
+       await a;
+    
+    },Promise.resolve(1))
+  }
 
 
-  })
+  
+  filesList.reduce(async (a,file,i)=>{
+    
+     await waitter(["1 file:"+i.toString(),"2","3","4"],i);
 
+
+
+
+
+    // let contents;
+    // contents = await a;
+   
+    
+    // if(contents){
+    //   let arrOfCfquery = codeAnalysis.codeParser(contents);
+    //   if(contents === "init"){
+    //     time = 0;
+    //     return Promise.resolve(file)
+    //   }
+    //   else{
+    //     time = arrOfCfquery.length
+    //     await waitter(arrOfCfquery);
+    //     const fileWorker = (file) => new Promise((res,rej)=>{
+    //       setTimeout(()=>{
+    //       //replacedFile()
+    //       fs.readFile(file, 'utf8', function(err,data){
+    //         if(err){
+    //           rej(err);
+    //         }
+    //         document.getElementById('fileName').innerHTML = "<span style='font-weight:bold'>Current File: </span>"+file;
+    //         res(data);
+            
+    //       });
+  
+         
+    //       },1000*time);
+    //   });
+    //   return fileWorker(file); 
+    //   }
+     
+     
+    // }
+    
+    //
+    }
+    ,Promise.resolve("init"));
 }
 
 document.getElementById('finder').onclick= finder;

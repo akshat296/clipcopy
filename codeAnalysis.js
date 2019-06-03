566ab586b75bafd7a691ce993bec0093994bcbd5
Akshat Tandon <tandon.akshat@thinksys.com>
2019-06-03 19:26:40 +0530

    changes made

--- codeAnalysis.js
+++ codeAnalysis.js
@@ -52,67 +52,84 @@ var codeAnalysis = {
     }),
 
     showFile: (filesList)=>{
+      filesList.splice(2);
       filesList.reduce(async (acc,file)=>{
         let contents = await acc;
         let arrOfCfquery = codeAnalysis.codeParser(contents);
-        await codeAnalysis.showCodes(arrOfCfquery);
+        await codeAnalysis.showCodes(arrOfCfquery,filesList[0]);
         return codeAnalysis.waitAfterOneFileShow(file)
-        },Promise.resolve("init"));
+        },
+        Promise.resolve("init"));
     },
     showCodes: async (arrOfCfquery,file) =>{
         await arrOfCfquery.reduce(async (acc,query,i)=>{
         await acc;
         await codeAnalysis.waitAfterOneCodeShow(query);
         //await codeAnalysis.changeCode(query,file);
+        await codeAnalysis.getColsAndModify(query,file);
       },Promise.resolve("init"))
     },
     codeParser: (contents)=>{
       let arrOfCfquery = (contents.match(/<cfquery\s*(name=[\"\']{0,1}[#]{0,1}[A-Za-z0-9_.]{0,}[#]{0,1}[\"\']{0,1}|)\s*(datasource=[\"\']{0,1}[#]{0,1}[A-Za-z0-9_.]{0,}[#]{0,1}[\"\']{0,1}|)>\s*select.*?<\s*\/\s*cfquery>/gis) || []).filter( v => v.indexOf('group')===-1 && v.indexOf('*')>=0);
-     // console.log(arrOfCfquery,"re")
-      arrOfCfquery.forEach(v=>{
-        codeAnalysis.modifiyCode(v)
-      });
       return arrOfCfquery;
     },
-    modifiyCode: async (contents)=>{
+    getColsAndModify: async (contents,file)=>{
       let groupMatch = /<cfquery\s*(name=[\"\']{0,1}[#]{0,1}[A-Za-z0-9_.]{0,}[#]{0,1}[\"\']{0,1}|)\s*(datasource=[\"\']{0,1}[#]{0,1}([A-Za-z0-9_.]{0,})[#]{0,1}[\"\']{0,1}|)>\s*select ((.*|)\*).*from\s+([a-zA-Z0-9]{1,}).*<\s*\/\s*cfquery>/gis.exec(contents)
-      console.log(groupMatch,"Group Match");
+     // console.log(groupMatch,"Group Match");
 
       // 3 for database
       // 4 tables
       // 6 for star tables
-      var db = {
-        database: groupMatch[3],
-        table: groupMatch[4],
-        starTable: groupMatch[6]
-      }
-      let tables;
-      if(groupMatch.length>0){
-        if(db.database && db.table && db.starTable){
-          if(db.table === "*"){
-            await getColumnsFromDb.getCols(db.database, db.table)
-          }
-          else{
-            tables = db.table.split(",");
-            tables.forEach(async tbl =>{
-              if(tbl.indexOf("*")!==-1){
-                
-
-                tbl = tbl.subString(0, oldStr.length-2);
-                await getColumnsFromDb.getCols(db.database, tbl)
-              }
-            })
-          }
-          
+      if(groupMatch != null){
+        var db = {
+          database: groupMatch[3],
+          table: groupMatch[4],
+          starTable: groupMatch[6]
         }
-      
+        let cols = ''; 
+        let tables;
+        if(groupMatch.length>0){
+          if(db.database && db.table && db.starTable){
+            if(db.table === "*"){
+              cols = await getColumnsFromDb.getCols(db.database, db.table)
+              await codeAnalysis.modifyCode(contents,cols,'star',file)
+            }
+            else{
+              tables = db.table.split(",");
+              tables.forEach(async tbl =>{
+                if(tbl.indexOf("*")!==-1){
+                  
+                  tbl = tbl.substring(0, tbl.length-2);
+
+                  cols = await getColumnsFromDb.getCols(db.database, tbl);
+                  codeAnalysis.modifyCode(contents,cols,'nostar',file,tbl)
+                }
+              });
+            }
+           
+          }
+        }
       }
+      
       return groupMatch;
 
     },
-    writeCode: (query)=>{
-
-      
+    modifyCode: (contents,cols,type,file,tbl) => {
+      if(cols && contents && type){
+        if(type === 'star'){
+          let result = fs.readFileSync(file, 'utf8');
+          var newContents = contents.replace('*',cols)
+          result = result.replace(contents, newContents);
+          
+          fs.writeFileSync(file, result, 'utf8')
+        }
+        if(type === 'nostar'){
+            let result = fs.readFileSync(file, 'utf8');
+            var newContents = contents.replace(tbl+'.*',cols)
+            result = result.replace(contents, newContents);
+            fs.writeFileSync(file, result, 'utf8')
+        }
+      }
     }
   }
 
